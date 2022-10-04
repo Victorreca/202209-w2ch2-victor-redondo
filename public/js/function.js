4fd593be47b317d338b4219394e5751d0bf929e6
Victor Redondo <vrviktor@gmail.com>
2022-10-04 19:35:19 +0200

    Resolve challenge 2

---
+++ public/js/function.js
@@ -0,0 +1,16 @@
+const strictEquals = (numberOne, numberTwo) => {
+  if (isNaN(numberOne) && isNaN(numberOne)) {
+    return false;
+  } else if (Object.is(numberOne, numberTwo)) {
+    return true;
+  } else if (
+    (Object.is(numberOne, 0) && Object.is(numberTwo, -0)) ||
+    (Object.is(numberTwo, 0) && Object.is(numberOne, -0))
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export default strictEquals;
